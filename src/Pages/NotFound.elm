134e0c000b46b2f609c26877655200daae27bd75
Farzeen <happycoder97@protonmail.com>
2019-10-15 09:29:47 +0530

    Not Found: update text

--- src/Pages/NotFound.elm
+++ src/Pages/NotFound.elm
@@ -17,5 +17,5 @@ view : Html.Html msg
 view =
     layout [] <|
         column [ Element.width (Element.px 400), Element.paddingXY 20 0, Element.centerX, Element.centerY, Font.size 15, Element.spacing 20 ]
-            [ Element.el (Theme.pageTitle ++ [ Element.moveUp 20.0 ]) (text "Page Not Found")
+            [ Element.el (Theme.pageTitle ++ [ Element.moveUp 20.0 ]) (text "Page not found")
             ]
