c04cd4526864d5a026cb3eaad238f790415acd2e
Farzeen <happycoder97@protonmail.com>
2019-10-15 06:13:09 +0530

    update Main.elm

--- src/Main.elm
+++ src/Main.elm
@@ -1,5 +1,6 @@
 module Main exposing (Model, Msg(..), init, main, update, view)
 
+import AppState exposing (AppState)
 import Backend exposing (Participant, getParticipants)
 import Browser
 import Browser.Navigation as Nav
@@ -11,9 +12,9 @@ import Html exposing (Html)
 import Html.Attributes exposing (class)
 import Html.Events exposing (onClick)
 import List
+import Pages
 import Pages.Login as Login
 import Pages.NotFound as NotFound
-import Route
 import Url
 
 
@@ -34,13 +35,28 @@ main =
 
 
 type Model
-    = LoginModel Login.Model
-    | PageNotFound
+    = Login Login.Model
+    | PageNotFound AppState
 
 
 init : flags -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
 init flags url navkey =
-    changeRouteTo (Route.toRoute <| Url.toString url) PageNotFound
+    let
+        appState =
+            AppState.init navkey
+
+        requestedRoute =
+            Route.toRoute <| Url.toString url
+
+        initialModel =
+            case AppState.getAuth appState of
+                AppState.LoggedIn _ ->
+                    PageNotFound appState
+
+                AppState.LoggedOut ->
+                    Login (Login.init appState)
+    in
+    ( initialModel, Cmd.none )
 
 
 
@@ -57,30 +73,49 @@ update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case ( msg, model ) of
         ( UrlChanged url, _ ) ->
-            changeRouteTo (Route.toRoute <| Url.toString url) model
+            loadRoute (Route.toRoute <| Url.toString url) model
 
         ( UrlRequested _, _ ) ->
             ( model, Cmd.none )
 
-        ( LoginMsg subMsg, LoginModel subModel ) ->
+        ( LoginMsg subMsg, Login subModel ) ->
             let
                 ( subModel_, subCmd ) =
                     Login.update subModel subMsg
             in
-            ( LoginModel subModel_, Cmd.map LoginMsg subCmd )
+            ( Login subModel_, Cmd.map LoginMsg subCmd )
 
-        ( _, PageNotFound ) ->
+        ( _, PageNotFound _ ) ->
             ( model, Cmd.none )
 
 
 changeRouteTo : Route.Route -> Model -> ( Model, Cmd Msg )
 changeRouteTo route model =
+    let
+        navKey =
+            AppState.getNavKey (getAppState model)
+    in
+    ( model, Route.redirectTo navKey route )
+
+
+loadRoute : Route.Route -> Model -> ( Model, Cmd Msg )
+loadRoute route model =
     case route of
         Route.Login ->
-            ( LoginModel Login.init, Cmd.none )
+            ( Login (Login.init <| getAppState model), Cmd.none )
 
         Route.NotFound ->
-            ( PageNotFound, Cmd.none )
+            ( PageNotFound <| getAppState model, Cmd.none )
+
+
+getAppState : Model -> AppState
+getAppState model =
+    case model of
+        Login subModel ->
+            Login.getAppState subModel
+
+        PageNotFound appState ->
+            appState
 
 
 
@@ -89,13 +124,14 @@ changeRouteTo route model =
 
 view : Model -> Browser.Document Msg
 view model =
-    { title = "Hello World"
+    { title = "Hospitality Admin"
     , body =
         [ case model of
-            LoginModel subModel ->
-                Html.map LoginMsg (Login.view subModel)
+            Login subModel ->
+                Login.view subModel
+                    |> Html.map LoginMsg
 
-            PageNotFound ->
+            PageNotFound _ ->
                 NotFound.view
         ]
     }
